d9e0749cca19f99982006e398907f0b24b07eb5f
William Wu <willwu1117@gmail.com>
2018-02-10 22:05:38 -0500

    gps

--- SwipeMeProject/App.js
+++ SwipeMeProject/App.js
@@ -1,80 +1,133 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Image, Text, Button, AppRegistry, SectionList } from 'react-native';
+import {AppRegistry, StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
 
-import t from 'tcomb-form-native'; // 0.6.9
-
-const Form = t.form.Form;
-
-const User = t.struct({
-  name: t.String,
-  email: t.String,
-  password: t.String,
-  phone: t.String
-});
+const {width, height} = Dimensions.get('window')
+const SCREEN_HEIGHT = height
+const SCREEN_WIDTH = width
+const ASPECT_RATIO = width / height
+const LATTITUDE_DELTA = 0.0922
+const LONGITUDE_DELTA = LATTITUDE_DELTA * ASPECT_RATIO
 
 export default class App extends Component {
-  handleSubmit = () => {
-    const value = this._form.getValue(); // use that ref to get the form value
-    console.log('value: ', value);
-  }
-  render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
+  constructor(props) {
+    super(props)
 
+    this.state = {
+      initialPosition: {
+        latitude:0,
+        longitude:0,
+        latitudeDelta:0,
+        longitudeDelta:0,
+      },
+      markerPosition: {
+        latitude:0,
+        longitude:0
+      }
+    }
+  }
+
+  watchID: ?number = null
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      var lat = parseFloat(position.coords.latitude)
+      var long = parseFloat(position.coords.longitude)
+      var initialRegion = {
+        latitude: lat,
+        longitude: long,
+        latitudeDelta: LATTITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      }
+
+      this.setState({initialPosition: initialRegion})
+      this.setState({markerPosition: initialRegion})
+
+    },
+    (error) => alert(JSON.stringify(error)),
+    {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000})
+
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      var lat = parseFloat(position.coords.latitude)
+      var long = parseFloat(position.coords.longitude)
+
+      var lastRegion = {
+        latitude: lat,
+        longitude: long,
+        longitudeDelta: LONGITUDE_DELTA,
+        latitudeDelta: LATTITUDE_DELTA
+      }
+
+      this.setState({initialPosition: lastRegion})
+      this.setState({markerPosition: lastRegion})
+    })
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID)
+  }
+
+  render() {
     return (
       <View style={styles.container}>
-        <Text style={{fontSize:50, textAlign: 'center'}}> Profile
-        </Text>
-        <Image source={pic} style={styles.image}/>
-        <SectionList styles={{marginTop:50}}
-          sections={[
-            {title: 'Name', data: ['Devin']},
-            {title: 'Email', data: ['devinboy123@gatech.edu']},
-            {title: 'Phone', data: ['123-456-7890']},
-          ]}
-          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
-          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-          keyExtractor={(item, index) => index}
-        />
-        <Button
-          title="Change info"
-          onPress={this.handleSubmit}
-        />
+        <MapView
+          style={styles.map}
+          initialRegion={this.state.initialPosition}>
+            <MapView.Marker
+              coordinate={this.state.markerPosition}>
+              <View style={styles.radius}>
+                <View style={styles.marker}/>
+              </View>
+            </MapView.Marker>
+        </MapView>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  radius: {
+    height: 50,
+    width: 50,
+    borderRadius: 50/2,
+    overflow: 'hidden',
+    backgroundColor: 'rgba(0, 122, 255, 0.1)',
+    borderWidth: 1,
+    borderColor: 'rgba(0, 112, 255, 0.3)',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  marker: {
+    height:20,
+    width:20,
+    borderWidth:3,
+    borderColor:'white',
+    borderRadius:20/2,
+    overflow:'hidden',
+    backgroundColor:'#007AFF'
+  },
   container: {
+    flex:1,
     justifyContent: 'center',
-    marginTop: '5%',
-    padding: 20,
-    backgroundColor: '#ffffff',
+    alignItems:'center',
+    backgroundColor: '#F5FCFF'
   },
-  image: {
-    height: 200,
-    borderRadius: 100,
-    width: 200,
-    alignSelf: 'center'
+  map: {
+    position: 'absolute',
+    top:0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
-  sectionHeader: {
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    fontSize: 14,
-    fontWeight: 'bold',
-    backgroundColor: 'rgba(247,247,247,1.0)',
+  welcome: {
+    fontSize:20,
+    textAlign: 'center',
+    margin:10
   },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   }
 });
-
-
-
 
