d13dbfe270dcfba30a37a143a8867bf0b969b30f
William Wu <willwu1117@gmail.com>
2018-02-10 23:50:13 -0500

    transaction

---
+++ SwipeMeProject/getter.js
@@ -0,0 +1,7 @@
+class getters {
+
+
+    getName(user person) {
+        return user.name;
+    }
+}
--- SwipeMeProject/transaction.js
+++ SwipeMeProject/transaction.js
@@ -64,4 +64,3 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between'
   }
 });
-
